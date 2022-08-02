a02ec7df0ad0e1b5cb90e1977dd06cb1f4eaf8cb
PolinaRrr <poniapocalypsisa@gmail.com>
2022-08-02 15:10:22 +0300

    #1
     View Binding library was connected

--- app/src/main/java/com/example/gb_hw_libraries/MainActivity.kt
+++ app/src/main/java/com/example/gb_hw_libraries/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.gb_hw_libraries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gb_hw_libraries.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
