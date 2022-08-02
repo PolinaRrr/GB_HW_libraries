f6687e96d7527eb6f1e93ec9faf849999f02af3f
PolinaRrr <poniapocalypsisa@gmail.com>
2022-08-02 16:00:21 +0300

    #2
    The main code has been written that will be spread across the layers of the application architecture

--- app/src/main/java/com/example/gb_hw_libraries/MainActivity.kt
+++ app/src/main/java/com/example/gb_hw_libraries/MainActivity.kt
@@ -6,11 +6,56 @@ import com.example.gb_hw_libraries.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
+    private val counter = mutableListOf(0, 0, 0)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        displayCounterValue()
+        with(binding) {
+            button01.setOnClickListener {
+                button01.text = increaseCounter(0).toString()
+            }
+            button02.setOnClickListener {
+                button01.text = increaseCounter(1).toString()
+            }
+            button03.setOnClickListener {
+                button01.text = increaseCounter(2).toString()
+            }
+        }
 
+    }
+
+    private fun increaseCounter(position: Int) {
+        ++counter[position]
+    }
+
+    private fun displayCounterValue() {
+        with(binding) {
+            button01.text = counter[0].toString()
+            button02.text = counter[1].toString()
+            button03.text = counter[2].toString()
+        }
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putIntArray(KEY_COUNTERS, counter.toIntArray())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        val array = savedInstanceState.getIntArray(KEY_COUNTERS)
+        counter.let { list->
+            list.clear()
+            array?.toList()?.let {
+                list.addAll(it)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/gb_hw_libraries/Utils.kt
@@ -0,0 +1,6 @@
+package com.example.gb_hw_libraries
+
+const val KEY_COUNTERS = "counters"
+
+class Utils {
+}
